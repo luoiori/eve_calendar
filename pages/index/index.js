3033e50ca117d9492afa84e6f11e439962e1e0e9
iori <iori.law.cn@icloud.com>
2019-08-06 14:44:05 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,16 +3,31 @@
 const app = getApp()
 const MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'June.', 'July.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
 Page({
-
-  saveConf: function (event){
+  saveConf: function (e){
     console.log("save conf")
     var _m = wx.getStorageSync("_firstDate_month")
     var _d = wx.getStorageSync("_firstDate_day")
+    var _y = wx.getStorageSync("_firstDate_year")
+    var key = _y+"_"+_m+"_"+_d
+    // 保存到服务器
+    var openid = wx.getStorageSync("openid")
+    if(openid){
+      wx.request({
+        url: 'https://redis.eveve.cc/set/' + openid + "/" + key,
+        success: function (res){
+          console.log(res.data)
+        }
+      })
+    }
+    
     if(_m){
       wx.setStorageSync("firstDate_month", _m)
     }
     if(_d){
       wx.setStorageSync("firstDate_day", _d)
+    }
+    if (_y) {
+      wx.setStorageSync("firstDate_year", _y)
     }
     wx.showToast({
       title: '修改成功',
@@ -21,18 +36,18 @@ Page({
     })
   },
   dayClick: function (event) {
+    console.log(event.detail)
     wx.setStorageSync("_firstDate_month", event.detail.month)
     wx.setStorageSync("_firstDate_day", event.detail.day)
+    wx.setStorageSync("_firstDate_year", event.detail.year)
     const fisrtDate = new Date(event.detail.year, event.detail.month-1, event.detail.day);
     console.log("firstdate="+fisrtDate)
     let demo5_days_style = new Array;
     const days_count = new Date(event.detail.year, event.detail.month - 1, 0).getDate();
     for (let i = 1; i <= days_count; i++) {
       const date = new Date(event.detail.year, event.detail.month - 1, i);
-      console.log(date)
       var diff = (date - fisrtDate) / (1000 * 60 * 60 * 24);
       var mod = diff % 9
-      console.log(mod+"->"+(mod+9))
       if (mod<0){
         mod += 9
       }
@@ -64,22 +79,74 @@ Page({
     demo6_days_style: [],
   },
   onLoad: function (options) {
+    // 判断有没有openid
+    var openid = wx.getStorageSync("openid")
+    if(!openid){
+      wx.login({
+        success(res) {
+          console.log(res.code)
+          if (res.code) {
+            wx.request({
+              url: 'https://redis.eveve.cc/openid',
+              data: { code: res.code },
+              method: 'POST',
+              header: {
+                'content-type': 'application/json'
+              },
+              success: function (res) {
+                if (res.statusCode == 200) {
+                  console.log("openid="+res.date)
+                  wx.setStorageSync('openid', res.data)
+                } else {
+                  console.log(res.errMsg)
+                }
+              },
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+    }
+
     // 假设2019-1-1是第0天
     var _m = 0
     var _d = 1
-    
-
-    var __m = wx.getStorageSync("firstDate_month")
-    if(__m){
-      _m = __m - 1
+    var _y = 2019
+    var reqSuccess = false;
+    if(openid){
+      wx.request({
+        url: 'https://redis.eveve.cc/get/' + openid,
+        success: function(res){
+          if (res.data!=""){
+            console.log(res.data)
+            var datas=res.data.split("_")
+            _y = datas[0]
+            _m = datas[1]
+            _d = datas[2]
+            reqSuccess = true
+          }
+        }
+      })
     }
 
-    var __d = wx.getStorageSync("firstDate_day")
-    if (__d) {
-      _d = __d
+    if(!reqSuccess){
+      var __m = wx.getStorageSync("firstDate_month")
+      if(__m){
+        _m = __m - 1
+      }
+
+      var __d = wx.getStorageSync("firstDate_day")
+      if (__d) {
+        _d = __d
+      }
+
+      var __y = wx.getStorageSync("firstDate_year")
+      if (__y) {
+        _y = __y
+      }
     }
-   
-    var fisrtDate = new Date(2019, _m, _d)
+    var fisrtDate = new Date(_y, _m, _d)
     
     const days_count = new Date(this.data.year, this.data.month-1, 0).getDate();
 
